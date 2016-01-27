27c5a022505d8093a6fb1ccd2039827bd147b938
Deathridge <danielrbetteridge@gmail.com>
2016-01-27 12:43:55 +0800

    added test data for astar testing

--- AStar.py
+++ AStar.py
@@ -32,17 +32,18 @@ def AStar(start, goal, nodes):
     while openList:
         current = min(openList, key=lambda o: o.g + o.h)
 
-        if current == goal:
+        if current.value == goal.value:
             pathTaken = []
             while current.parent:
                 pathTaken.append(current)
                 current = current.parent
             pathTaken.append(current)
-            return path[::-1]
+            
+            return pathTaken[::-1]
         
         successorList = successors(current,nodes)
         for successor in successorList:
-            successor.g = current.g + current.move_cost(successor)
+            successor.g = current.g + current.cost(successor)
             successor.h = manhattan(goal, successor)
             successor.parent = current
             addSuccessor = True
@@ -58,4 +59,17 @@ def AStar(start, goal, nodes):
             if addSuccessor:
                 openList.add(successor)
         openList.remove(current)
-        closed.add(current)   
+        closed.add(current)
+    raise ValueError('No path was found')
+
+sA = [Node(1,2,'B'), Node(2,1,'D')]
+sB = [Node(1,1,'A'), Node(2,2,'E'), Node(1,3,'C')]
+sC = [Node(1,2,'B'), Node(2,3,'F')]
+sD = [Node(1,1,'A'),Node(2,2,'E')]
+sE = [Node(3,2,'H'),Node(2,3,'F')]
+sF = [Node(2,2,'E'),Node(3,3,'I')]
+sG = [Node(2,1,'D'),Node(3,2,'H')]
+sH = [Node(3,1,'G')]
+sI = [Node(3,2,'H')]
+
+nodes = {'A':sA, 'B':sB, 'C':sC,'D':sD,'E':sE,'F':sF, 'G':sG, 'H':sH, 'I':sI}
